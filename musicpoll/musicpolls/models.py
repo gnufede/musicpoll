ba72595aa39551088053c4c102869bccfa276e00
Federico Mon <gnu.fede@gmail.com>
2013-12-01 14:44:18 +0100

    models, models

--- musicpoll/musicpolls/models.py
+++ musicpoll/musicpolls/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Song(models.Model):
-    lasturl = models.CharField(max_length=500)
     name = models.CharField(max_length=500)
     artist = models.CharField(max_length=500)
+    lasturl = models.CharField(max_length=500)
 
     def __unicode__(self):
-        return self.artist +' - '+ self.name
+        return self.name + ' - ' + self.artist
 
 class Choice(models.Model):
     user = models.ForeignKey(User)
+    index = models.IntegerField(default=1,
+                    validators=[MinValueValidator(1), MaxValueValidator(10)])
     song = models.ForeignKey(Song)
-    index = models.IntegerField(default=0)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.user.username +' ('+str(self.index)+'): ' + str(self.song)
+
