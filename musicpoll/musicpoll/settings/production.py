650d4f5b44fbd130c4a63141f9f364133b5e98a2
Fede <gnu.fede@gmail.com>
2013-12-09 13:17:20 +0100

    changes in production settings to run on heroku

--- musicpoll/musicpoll/settings/production.py
+++ musicpoll/musicpoll/settings/production.py
@@ -50,8 +50,25 @@ SERVER_EMAIL = EMAIL_HOST_USER
 ########## END EMAIL CONFIGURATION
 
 ########## DATABASE CONFIGURATION
-DATABASES = {}
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 ########## END DATABASE CONFIGURATION
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 ########## CACHE CONFIGURATION
