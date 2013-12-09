6415d5334477baeff9b614119e927defccbf88bb
Fede <gnu.fede@gmail.com>
2013-12-09 16:52:06 +0100

    settings imported

--- musicpoll/musicpoll/urls.py
+++ musicpoll/musicpoll/urls.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
