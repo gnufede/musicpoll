04d6d12cd81345c2014d561f17f3d450d13bf030
Fede <gnu.fede@gmail.com>
2013-12-09 17:06:01 +0100

    changes in production settings

--- musicpoll/musicpoll/settings/production.py
+++ musicpoll/musicpoll/settings/production.py
@@ -63,11 +63,11 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(SITE_ROOT, '/static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(SITE_ROOT, 'static'),
 )
 
 
